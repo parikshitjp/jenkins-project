6599c36593c7f18850fd7cba36a5f425a079c592
Ubuntu <ubuntu@ip-172-31-8-206.us-east-2.compute.internal>
2024-08-14 16:50:12 +0000

    Excellent

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to Parikshit Devops ");
+        System.out.println("Welcome to Parikshit's Devopsified Pro ");
     }
 }
 
